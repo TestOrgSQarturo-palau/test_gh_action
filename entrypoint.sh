911db1a314a6ca4504e1f6bae60dc8c2ba8eb269
Arturo Palau <132499885+arturo-palau-sonarsource@users.noreply.github.com>
2024-08-02 09:39:28 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 command="echo Checking current status of project in SonarQube"
-response=$(curl -u ${{ $1 }}: -X GET '${{ $2 }}api/alm_settings/get_binding?project=${{ $3 }}')
+response=$(curl -u $1: -X GET '$2/api/alm_settings/get_binding?project=$3')
 echo "response=$response" >> $GITHUB_ENV
 echo "response=$response"
 
