8649881a9d1b75d4955f7c6ebc3579d631c3b434
Arturo Palau <132499885+arturo-palau-sonarsource@users.noreply.github.com>
2025-02-06 13:25:42 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+# Check if the required arguments are provided
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <SONAR_TOKEN> <SONAR_HOST_URL> <PROJECT_KEY>"
+  exit 1
+fi
+
+SONAR_TOKEN=$1
+SONAR_HOST_URL=$2
+PROJECT_KEY=$3
+
+# Command to check the current status of the project in SonarQube
 command="echo Checking current status of project in SonarQube"
-response=$(curl -u $1: -X GET '$2/api/alm_settings/get_binding?project=$3')
-echo "response=$response" >> value.txt
-echo "value.txt" >> $GITHUB_ENV
-#echo "response=$response"
-
-
 eval $command
+
+# Make the API call to SonarQube
+response=$(curl -u $SONAR_TOKEN: -X GET "$SONAR_HOST_URL/api/alm_settings/get_binding?project=$PROJECT_KEY")
+
+# Save the response to a file
+echo "response=$response" > value.txt
+
+# Set the file path as an environment variable
+echo "VALUE_FILE=value.txt" >> $GITHUB_ENV
