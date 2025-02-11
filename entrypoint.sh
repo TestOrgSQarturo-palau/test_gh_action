06931f6c99d5b378e330a1596b2a183c07d80e03
Arturo Palau <132499885+arturo-palau-sonarsource@users.noreply.github.com>
2025-02-11 13:17:48 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,13 +12,13 @@ echo "Repository Key used: $REPO_KEY"
 echo "SonarQube DevOps Integration Name: $SQ_DEVOPS_INTEGRATION_NAME"
 
 # Make the API call to SonarQube
-response=$(curl -u $SONAR_TOKEN: -X GET "$SONAR_HOST_URL/api/alm_settings/get_binding?project=$PROJECT_KEY")
+response=$(curl -s -u $SONAR_TOKEN: -X GET "$SONAR_HOST_URL/api/alm_settings/get_binding?project=$PROJECT_KEY")
 echo "response=$response" 
 
 case $response in
     *"is not bound to any DevOps Platform"*)
     echo "The condition was met, adding the alm binding."
-    curl -u $SONAR_TOKEN: -X POST "$SONAR_HOST_URL/api/alm_settings/set_github_binding?almSetting=$SQ_DEVOPS_INTEGRATION_NAME&monorepo=false&project=$PROJECT_KEY&repository=$REPO_KEY"
+    curl -s -u $SONAR_TOKEN: -X POST "$SONAR_HOST_URL/api/alm_settings/set_github_binding?almSetting=$SQ_DEVOPS_INTEGRATION_NAME&monorepo=false&project=$PROJECT_KEY&repository=$REPO_KEY"
       ;;
     *)
       echo "The condition was not met, so the task was not executed."
